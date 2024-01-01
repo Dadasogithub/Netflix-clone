53c0bbc6baa23634298a16e33d200bf7299bfcea
dadaso <101014255+Dadasogithub@users.noreply.github.com>
2024-01-01 19:56:14 +0530

    i created this project in 2021 Now a uploading on github

---
+++ script.js
@@ -0,0 +1,89 @@
+var a=1;
+function showorhide1(){
+    if (a==1){
+        document.getElementById("greybox1").style.display="flex";
+        document.getElementById("plus_btn_1").style.transform='rotate(45deg)'; 
+
+        return a=0;
+    }
+    else{
+        document.getElementById("greybox1").style.display="none";
+        document.getElementById("plus_btn_1").style.transform='rotate(90deg)'; 
+
+        return a=1;   
+    }
+}
+function showorhide2(){
+    if (a==1){
+        document.getElementById("greybox2").style.display="flex";   
+        document.getElementById("plus_btn_2").style.transform='rotate(45deg)'; 
+
+        return a=0;
+    }
+    else{
+        document.getElementById("greybox2").style.display="none";
+        document.getElementById("plus_btn_2").style.transform='rotate(90deg)'; 
+
+        return a=1;   
+    }
+}
+var a=1;
+function showorhide3(){
+    if (a==1){
+        document.getElementById("greybox3").style.display="flex";
+        document.getElementById("plus_btn_3").style.transform='rotate(45deg)'; 
+
+        return a=0;
+    }
+    else{
+        document.getElementById("greybox3").style.display="none";
+        document.getElementById("plus_btn_3").style.transform='rotate(90deg)'; 
+
+        return a=1;   
+    }
+}
+var a=1;
+function showorhide4(){
+    if (a==1){
+        document.getElementById("greybox4").style.display="flex";
+        document.getElementById("plus_btn_4").style.transform='rotate(45deg)'; 
+
+        return a=0;
+    }
+    else{
+        document.getElementById("greybox4").style.display="none";
+        document.getElementById("plus_btn_4").style.transform='rotate(90deg)'; 
+
+        return a=1;   
+    }
+}
+var a=1;
+function showorhide5(){
+    if (a==1){
+        document.getElementById("greybox5").style.display="flex";
+        document.getElementById("plus_btn_5").style.transform='rotate(45deg)'; 
+
+        return a=0;
+    }
+    else{
+        document.getElementById("greybox5").style.display="none";
+        document.getElementById("plus_btn_5").style.transform='rotate(90deg)'; 
+
+        return a=1;   
+    }
+}
+var a=1;
+function showorhide6(){
+    if (a==1){
+        document.getElementById("greybox6").style.display="flex";
+        document.getElementById("plus_btn_6").style.transform='rotate(45deg)'; 
+
+        return a=0;
+    }
+    else{
+        document.getElementById("greybox6").style.display="none";
+        document.getElementById("plus_btn_6").style.transform='rotate(90deg)'; 
+
+        return a=1;   
+    }
+}
